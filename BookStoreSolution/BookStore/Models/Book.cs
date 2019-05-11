58b369d509b0e39fec9682d02f163a25c41e4ab9
HamletPoghosian <phamik@mail.ru>
2019-05-11 15:35:10 +0400

    add new function

--- BookStoreSolution/BookStore/Models/Book.cs
+++ BookStoreSolution/BookStore/Models/Book.cs
@@ -8,7 +8,7 @@ namespace BookStore.Models
 {
     public class Book
     {
-        
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
         public DateTime PublishDate { get; set; }
