bb57c18db72c37412a2cad9b42e19bebae59ff70
HamletPoghosian <phamik@mail.ru>
2019-05-14 14:03:05 +0400

    add new controler

--- BookStoreSolution/BookStore/ViewModels/RegisterModel.cs
+++ BookStoreSolution/BookStore/ViewModels/RegisterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,15 @@ namespace BookStore.ViewModels
 {
     public class RegisterModel
     {
+        [Required(ErrorMessage = "No Email specified")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "No Password specified")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Invalid password")]
+        public string ConfirmPassword { get; set; }
     }
 }
