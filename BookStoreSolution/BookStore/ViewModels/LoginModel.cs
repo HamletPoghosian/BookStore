86909ebde388765e5bb1aaf83178a3d7c1507cc8
HamletPoghosian <phamik@mail.ru>
2019-05-14 14:04:07 +0400

    add new prop

--- BookStoreSolution/BookStore/ViewModels/LoginModel.cs
+++ BookStoreSolution/BookStore/ViewModels/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace BookStore.ViewModels
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "No Email specified")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "No Password specified")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
