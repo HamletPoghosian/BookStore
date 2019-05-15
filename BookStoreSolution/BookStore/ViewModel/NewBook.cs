ca81c8db78bd53dec3b29357e50d8694457fca38
HamletPoghosian <phamik@mail.ru>
2019-05-15 16:14:27 +0400

    add new class

---
+++ BookStoreSolution/BookStore/ViewModel/NewBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.ViewModel
+{
+    public class NewBook
+    {
+       
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int Price { get; set; }
+        public int Popular { get; set; }
+    }
+}
