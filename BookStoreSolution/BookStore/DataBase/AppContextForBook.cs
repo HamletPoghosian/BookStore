112c4f2e51e4b3cc0cea20e5d9bae568dae370a8
HamletPoghosian <phamik@mail.ru>
2019-05-16 19:04:50 +0400

    add new function

--- BookStoreSolution/BookStore/DataBase/AppContextForBook.cs
+++ BookStoreSolution/BookStore/DataBase/AppContextForBook.cs
@@ -7,11 +7,28 @@ namespace BookStore.Data
 
     public class AppContextForBook : DbContext
     {
+        public AppContextForBook(DbContextOptions<AppContextForBook> options) : base(options)
+        { }
         public DbSet<Book> Books { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
                   @"Server=(localdb)\mssqllocaldb;Database=Book_EFCore;Integrated Security=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(e =>
+            {
+                e.HasKey(b => b.Id);
+                e.Property(b => b.Id).HasDefaultValueSql("(newid())");
+                e.Property(b => b.Name).IsRequired();
+                e.Property(b => b.Author).IsRequired().HasDefaultValue(false);
+                e.Property(b => b.Popular).IsRequired(); ;
+                e.Property(b => b.Price).IsRequired();
+                e.Property(b => b.PublishDate).IsRequired();
+            });
+
+            base.OnModelCreating(modelBuilder);
         }
 
     }
